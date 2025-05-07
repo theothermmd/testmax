03bb2a7c36199762245cc08f86b39ee2216ea32c
Mohamadreza <161870762+theothermmd@users.noreply.github.com>
2025-05-08 02:32:47 +0330

    سشی

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 "use strict";
 
 import Fastify from "fastify";
-import {find_best_route} from "./utils/main.js";
+import {findBestRoute} from "./utils/main.js";
 
 const app = Fastify({
   logger: true,
@@ -10,7 +10,7 @@ const app = Fastify({
 
 app.post('/route', async (request) => {
     const { source, destination , type_day , time } = request.body;
-    const res = await find_best_route(source , destination , type_day , time);
+    const res = await findBestRoute(source , destination , type_day , time);
     return { message: res };
 });
 
